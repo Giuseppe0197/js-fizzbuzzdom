94670740ac734e93312ff9831ca609b5b5e664cd
Giuseppe0197 <faro.g@hotmail.it>
2021-10-25 15:19:53 +0200

    rimossi numeri non necessari

--- js/myscript.js
+++ js/myscript.js
@@ -2,9 +2,6 @@
 
 const containerSquare = document.getElementById("containerQuadrati");
 
-/* const square = document.createElement("div");
-square.classList.add("quadrati"); */
-
 /* andiamo a creare il ciclo per inserire i quadrati */
 
 for (let i = 1; i <= 100; i++){
@@ -17,20 +14,22 @@ for (let i = 1; i <= 100; i++){
     square.append(i);
     containerSquare.append(square);
 
+    /* poniamo le condizioni per i diversi colori dei quadrati */
+
      if ((i % 3 === 0) && (i % 5 === 0)) {
 
         square.classList.add("quadratiFizzBuzz");
-        square.append(`FizzBuzz`);
+        square.innerHTML = `FizzBuzz`;
 
     } else if (i % 3 === 0) {
 
         square.classList.add("quadratiFizz");
-        square.append(`Fizz`);
+        square.innerHTML = `Fizz`;
 
     } else if (i % 5 === 0) {
 
         square.classList.add("quadratiBuzz");
-        square.append(`Buzz`);
+        square.innerHTML = `Buzz`;
 
     }
 
