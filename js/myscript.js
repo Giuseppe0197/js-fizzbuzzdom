9e4f6dd204b8fb04d4677788ff0c06a6242479c6
Giuseppe0197 <faro.g@hotmail.it>
2021-10-25 15:02:51 +0200

    quadrati fizz e buzz

---
+++ js/myscript.js
@@ -0,0 +1,37 @@
+/* definiamo le variabili */
+
+const containerSquare = document.getElementById("containerQuadrati");
+
+/* const square = document.createElement("div");
+square.classList.add("quadrati"); */
+
+/* andiamo a creare il ciclo per inserire i quadrati */
+
+for (let i = 1; i <= 100; i++){
+
+    /* const square = `<div class="quadrati quadrati--${i}">${i}</div>`
+    containerSquare.innerHTML += square; */
+
+    const square = document.createElement("div");
+    square.classList.add("quadrati");
+    square.append(i);
+    containerSquare.append(square);
+
+    if (i % 3 === 0) {
+
+        square.classList.add("quadratiFizz");
+        square.append(`Fizz`);
+
+    } else if (i % 5 === 0) {
+
+        square.classList.add("quadratiBuzz");
+        square.append(`Buzz`);
+
+    } else if ((i % 3 === 0) && (i % 5 === 0)) {
+
+        square.classList.add("quadratiFizzBuzz");
+        square.append(`FizzBuzz`);
+
+    }
+
+}
